7b2e5fa40c710b9d038c6eb0fcdf6a9091622e67
Vitorio-Max <vitorrino33@gmail.com>
2025-08-10 16:20:09 +0200

    vv

--- JavaScript/supabase.js
+++ JavaScript/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js';
 
 // Reemplaza con tu URL y tu clave anon
 const supabaseUrl = 'https://usbrxrzekdmktdiiyqzq.supabase.co';
